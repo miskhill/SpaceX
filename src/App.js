f8d0e9ccf6294800ba8d6c4dfd9a0e31ef01bd91
garysmith <miskhill@hotmail.com>
2021-12-02 21:42:22 +0000

    graphql trial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
+//vaid query from the GraphQL site for launches - save to a variable
+const LAUNCHES_QUERY = `
+{
+  launchesPast(limit: 10) {
+    id
+    mission_name
+  }
+}
+`
+const SHIP_QUERY = `
+{
+  ships {
+    image
+  }
+}
+`
+
+//use the fetch API built into the browser - Axios would also work here 
+
+export default function App() {
+
+const [launches, setLaunches] = React.useState([])
+const [ships, setShips] = React.useState([])
+
+console.log(setShips)
+  
+React.useEffect(() => {
+    fetch('https://api.spacex.land/graphql/', {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({query: LAUNCHES_QUERY})
+    }).then((response) => response.json())
+      .then(data => setLaunches(data.data.launchesPast))
+      
+  }, [])
+ 
+  React.useEffect(() => {
+    
+    fetch('https://api.spacex.land/graphql/', {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({query: SHIP_QUERY})
+    }).then((response) => response.json())
+      .then(data => console.log(data))
+    
+      
+  }, [])
+  
+  //setShips(data.ships)
+  //console.log(data)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>CloudShelf GraphQL fetch example from SpaceX Graphql api</h1>
     </div>
-  );
+      <ul>
+      {launches.map((launch) => (
+        <li key={launch.id}>{launch.mission_name} {ships.image}</li>
+      ))}
+      </ul>
+    </>
+  )
+
 }
 
-export default App;
